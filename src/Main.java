f4d83c84627da4d2f49969a39046fd29621312a4
SunflowerKun <dream26322gmail.com>
2022-05-01 17:26:32 +0600

    new file was added

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+    public static void main( String[] args ){
+        int a = 1;
+        System.out.println("Bismillah!");
+        while (a<34){
+            System.out.println(a+" Alhamdulillah");
+            System.out.println(a+" SubhanAllah");
+            System.out.println(a+" AllahuAkbar");
+            a++;
+        }
+
+
+    }
+}
